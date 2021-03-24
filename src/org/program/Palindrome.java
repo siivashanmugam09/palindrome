800923747c51b133e8fd7ce499e0d02a78ca1223
sivashanmugam09 <sivashanmugam.s01@gmail.com>
2021-03-24 14:50:41 +0530

    first

---
+++ src/org/program/Palindrome.java
@@ -0,0 +1,28 @@
+package org.program;
+
+import java.util.Scanner;
+
+public class Palindrome {
+public static void main(String[] args) {
+	int number = 121;
+	int res = 0;
+	int rem =0;
+	int n = number;
+//	System.out.println("enter number");
+//	Scanner sc = new Scanner(System.in);
+//	int n = sc.nextInt();
+	
+	
+	while (number>0) {
+		rem = number%10;
+		res = rem+(res*10);
+		number=number/10;
+	}
+	if ( n==res)
+		System.out.println("is palindrome");
+	else {
+		System.out.println("not palindrome");
+	}
+
+}
+}
